26d72cc6cd863673abbac2132e22e9f6b9196d1a
lewi252 <gabrielmostro32@gmail.com>
2025-09-25 22:21:44 -0300

    colocomos route de admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('conteudo')
+    <h1>Bem-vindo ao Lambida Doce!</h1>
+
+    <p>
+        Explore nossos deliciosos produtos e
+        aproveite oferta exclusivas.
+    </p>
+
+<a href="#">Admin</a>
+@endsection
